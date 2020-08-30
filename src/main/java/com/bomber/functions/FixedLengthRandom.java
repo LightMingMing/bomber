bb1b3646b4095a6e703a5d2f07d3a21793f4bc55
LightMingMing <1849901944@qq.com>
2020-08-30 12:34:10 +0800

    Add function: FixedLengthRandom

---
+++ src/main/java/com/bomber/functions/FixedLengthRandom.java
@@ -0,0 +1,54 @@
+package com.bomber.functions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.bomber.functions.core.FuncInfo;
+import com.bomber.functions.core.Input;
+import com.bomber.functions.core.StringFunction;
+
+@FuncInfo(requiredArgs = "length")
+public class FixedLengthRandom extends StringFunction {
+
+	// Long.MAX_VALUE length is 19
+	private static final int MAX_LENGTH = Long.toString(Long.MAX_VALUE).length();
+
+	private int length;
+
+	private List<Integer> lengthSplits;
+
+	private static Long random(int length) {
+		long min = (long) Math.pow(10, length - 1);
+		long max = (length == MAX_LENGTH) ? Long.MAX_VALUE : (long) Math.pow(10, length);
+		return ThreadLocalRandom.current().nextLong(min, max);
+	}
+
+	@Override
+	public void init(Input input) {
+		this.length = Integer.parseInt(input.get("length"));
+		this.lengthSplits = new ArrayList<>(1);
+
+		int temp = this.length;
+		for (;;) {
+			if (temp > MAX_LENGTH) {
+				temp -= MAX_LENGTH;
+				lengthSplits.add(MAX_LENGTH);
+			} else {
+				lengthSplits.add(temp);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public String execute(Input ctx) {
+		if (this.length <= MAX_LENGTH) {
+			return Long.toString(random(this.length));
+		} else {
+			StringBuilder sb = new StringBuilder();
+			lengthSplits.forEach(len -> sb.append(random(len)));
+			return sb.toString();
+		}
+	}
+}
