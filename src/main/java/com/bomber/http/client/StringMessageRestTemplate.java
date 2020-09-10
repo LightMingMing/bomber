d28f73b7cf943e548a985d2e6ec085655d08982d
LightMingMing <1849901944@qq.com>
2020-09-10 21:30:03 +0800

    Set timeout of 2 minutes

--- src/main/java/com/bomber/http/client/StringMessageRestTemplate.java
+++ src/main/java/com/bomber/http/client/StringMessageRestTemplate.java
@@ -4,14 +4,23 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 import org.ironrhino.core.spring.http.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StringMessageRestTemplate extends RestTemplate {
 
+	private final int TIMEOUT = 1000 * 120; // 2 min
+
 	public StringMessageRestTemplate() {
 		super();
 		setMessageConverters(Collections.singletonList(new StringHttpMessageConverter(StandardCharsets.UTF_8)));
+	}
+
+	@Override
+	@Value("${stringMessageRestTemplate.readTimeout:" + TIMEOUT + "}")
+	public void setReadTimeout(int readTimeout) {
+		super.setReadTimeout(Math.max(readTimeout, TIMEOUT));
 	}
 }
