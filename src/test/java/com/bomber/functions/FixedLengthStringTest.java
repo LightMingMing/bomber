e7b160fd9243c7bfbc418b6623ec70f1f7dc82ab
LightMingMing <1849901944@qq.com>
2020-09-28 15:04:49 +0800

    Remove test case

--- src/test/java/com/bomber/functions/FixedLengthStringTest.java
+++ src/test/java/com/bomber/functions/FixedLengthStringTest.java
@@ -1,6 +1,5 @@
 package com.bomber.functions;
 
-import static com.bomber.functions.FixedLengthString.MAX_LENGTH;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.bomber.functions.core.Input;
@@ -20,17 +19,6 @@ public class FixedLengthStringTest {
 			func.execute(Input.EMPTY);
 		}
 		assertThat(func.execute(Input.EMPTY)).isEqualTo("2");
-	}
-
-	@Test
-	public void testMaxLength() {
-		FixedLengthString func = new FixedLengthString();
-
-		func.init(new Input("length", MAX_LENGTH + ""));
-		assertThat(func.execute(Input.EMPTY)).hasSize(MAX_LENGTH);
-
-		func.init(new Input("length", MAX_LENGTH + 1 + ""));
-		assertThat(func.execute(Input.EMPTY)).hasSize(MAX_LENGTH);
 	}
 
 }
