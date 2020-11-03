256ad50ed5a5806c2f6217feb1eadcfa5e010386
LightMingMing <1849901944@qq.com>
2020-11-03 10:14:06 +0800

    Fix NoClassDefFoundError

--- src/main/java/io/github/resilience4j/circuitbreaker/CallNotPermittedException.java
+++
@@ -1,9 +0,0 @@
-package io.github.resilience4j.circuitbreaker;
-
-// Fix java.lang.NoClassDefFoundError if resilience4j-all.jar is removed
-// TODO upgrade ironrhino
-public class CallNotPermittedException extends RuntimeException {
-
-	private static final long serialVersionUID = 4258982742865398968L;
-
-}
