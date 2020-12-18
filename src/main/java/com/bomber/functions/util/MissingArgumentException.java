7a0b333f05ecaddc42aef25ddcab4489c90228a5
LightMingMing <1849901944@qq.com>
2020-12-18 15:38:07 +0800

    Suppress warning

--- src/main/java/com/bomber/functions/util/MissingArgumentException.java
+++ src/main/java/com/bomber/functions/util/MissingArgumentException.java
@@ -2,6 +2,8 @@ package com.bomber.functions.util;
 
 public class MissingArgumentException extends RuntimeException {
 
+	private static final long serialVersionUID = 91339374763516624L;
+
 	private static final String format = "Function '%s' missing argument '%s'";
 
 	public MissingArgumentException(String function, String arg) {
