cec0660f0154f05f59110136f91d891be0b5ed45
LightMingMing <1849901944@qq.com>
2020-12-26 00:09:08 +0800

    Jetty hot deployment

--- src/main/java/com/bomber/JettyBootstrap.java
+++ src/main/java/com/bomber/JettyBootstrap.java
@@ -1,9 +1,13 @@
 package com.bomber;
 
+import java.util.Arrays;
+
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.deploy.DeploymentManager;
+import org.eclipse.jetty.deploy.providers.WebAppProvider;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.handler.ShutdownHandler;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.webapp.Configuration;
@@ -18,15 +22,30 @@ public class JettyBootstrap {
 		Configuration.ClassList classList = Configuration.ClassList.setServerDefault(server);
 		classList.add(AnnotationConfiguration.class.getName());
 
-		WebAppContext context = new WebAppContext();
-		context.setContextPath("/");
-		context.setResourceBase("./src/main/webapp");
-		context.setAttribute(WebInfConfiguration.CONTAINER_JAR_PATTERN, ".*/ironrhino-[^/]*\\.jar$");
-		context.setInitParameter(DefaultServlet.CONTEXT_INIT + "dirAllowed", "false");
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setContextPath("/");
+		webAppContext.setResourceBase("./src/main/webapp");
+		webAppContext.setAttribute(WebInfConfiguration.CONTAINER_JAR_PATTERN, ".*/ironrhino-[^/]*\\.jar$");
+		webAppContext.setInitParameter(DefaultServlet.CONTEXT_INIT + "dirAllowed", "false");
 
-		HandlerList handlers = new HandlerList();
-		handlers.setHandlers(new Handler[]{new ShutdownHandler("password"), context});
+		ContextHandlerCollection contexts = new ContextHandlerCollection();
 
+		HandlerCollection handlers = new HandlerCollection();
+		handlers.addHandler(new ShutdownHandler("password"));
+		handlers.addHandler(webAppContext);
+		handlers.addHandler(contexts);
+
+		WebAppProvider provider = new WebAppProvider();
+		provider.setMonitoredDirectories(
+				Arrays.asList("./src/main/webapp", "./src/main/resources", "./build/classes/java/main"));
+		provider.setExtractWars(false);
+		provider.setScanInterval(2);
+
+		DeploymentManager deployer = new DeploymentManager();
+		deployer.setContexts(contexts);
+		deployer.addAppProvider(provider);
+
+		server.addBean(deployer);
 		server.setHandler(handlers);
 		server.setStopAtShutdown(true);
 		server.start();
