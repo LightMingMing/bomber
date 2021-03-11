c2df65bbf26d736fc8ab943d1ff0edca0c9d421e
LightMingMing <1849901944@qq.com>
2021-03-11 11:57:03 +0800

    Improve fixedLengthString

--- src/main/java/com/bomber/functions/FixedLengthString.java
+++ src/main/java/com/bomber/functions/FixedLengthString.java
@@ -1,48 +1,82 @@
 package com.bomber.functions;
 
-import com.bomber.functions.core.Input;
 import com.bomber.functions.core.FuncInfo;
+import com.bomber.functions.core.Input;
 import com.bomber.functions.core.StringFunction;
 
 @FuncInfo(requiredArgs = "length", optionalArgs = "prefix, suffix")
 public class FixedLengthString extends StringFunction {
 
-	protected static final int MAX_LENGTH = 18;
+	protected static final int MAX_LENGTH = 64;
 
-	private String format;
+	protected static final int MAX_COUNT_LENGTH = 18;
+
+	private int length;
+
+	private String prefix;
+
+	private String suffix;
+
+	private int numberLength;
 
 	private long mod;
 
 	private long count;
 
+	private static int stringSize(long x) {
+		long p = 10;
+		for (int i = 1; i < MAX_COUNT_LENGTH; i++) {
+			if (x < p)
+				return i;
+			p = 10 * p;
+		}
+		return MAX_COUNT_LENGTH;
+	}
+
 	@Override
 	public void init(Input input) {
 		int length = Integer.parseInt(input.get("length"));
-		if (length < 1) {
-			throw new IllegalArgumentException("length should great than 0");
+		if (length < 1 || length > MAX_LENGTH) {
+			throw new IllegalArgumentException("length: " + length + ", (expected: 1-" + MAX_LENGTH + ")");
 		}
+		this.length = length;
 
-		String prefix = input.get("prefix");
-		String suffix = input.get("suffix");
+		this.prefix = input.get("prefix");
+		this.suffix = input.get("suffix");
 
-		StringBuilder sb = new StringBuilder();
+		int numberLength = length;
 		if (prefix != null) {
-			sb.append(prefix);
+			numberLength -= prefix.length();
 		}
-		sb.append("%0").append(length).append("d");
 		if (suffix != null) {
-			sb.append(suffix);
+			numberLength -= suffix.length();
 		}
-		this.format = sb.toString();
-
-		this.mod = (long) Math.pow(10, Math.min(length, MAX_LENGTH));
+		if (numberLength < 1) {
+			throw new IllegalArgumentException("numberLength: " + numberLength + ", (expected: > 0)");
+		}
+		this.numberLength = numberLength;
+		this.mod = (long) Math.pow(10, Math.min(this.numberLength, MAX_COUNT_LENGTH));
 	}
 
 	@Override
 	public String execute(Input input) {
-		String result = String.format(format, count);
+		StringBuilder sb = new StringBuilder(length);
+
+		if (prefix != null)
+			sb.append(prefix);
+
+		int paddedZeroLength = numberLength - stringSize(count);
+		if (paddedZeroLength > 0) {
+			sb.append("0".repeat(paddedZeroLength));
+		}
+
+		sb.append(count);
+
+		if (suffix != null)
+			sb.append(suffix);
+
 		count = (++count) % mod;
-		return result;
+		return sb.toString();
 	}
 
 	@Override
--- src/test/java/com/bomber/functions/FixedLengthStringTest.java
+++ src/test/java/com/bomber/functions/FixedLengthStringTest.java
@@ -1,15 +1,18 @@
 package com.bomber.functions;
 
+import static com.bomber.functions.FixedLengthString.MAX_LENGTH;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import com.bomber.functions.core.Input;
 import org.junit.Test;
+
+import com.bomber.functions.core.Function;
+import com.bomber.functions.core.Input;
 
 public class FixedLengthStringTest {
 
 	@Test
 	public void testFixedLength() {
-		FixedLengthString func = new FixedLengthString();
+		Function<String> func = new FixedLengthString();
 		func.init(new Input("length", "1"));
 
 		assertThat(func.execute(Input.EMPTY)).isEqualTo("0");
@@ -19,6 +22,27 @@ public class FixedLengthStringTest {
 			func.execute(Input.EMPTY);
 		}
 		assertThat(func.execute(Input.EMPTY)).isEqualTo("2");
+	}
+
+	@Test
+	public void testFixedLengthWithDecorator() {
+		Function<String> func = new FixedLengthString();
+		int length = 48;
+		String prefix = "FixedLength";
+		func.init(new Input("length", "" + length, "prefix", prefix));
+
+		String expected0 = prefix + "0".repeat(length - prefix.length());
+		assertThat(func.execute(Input.EMPTY)).isEqualTo(expected0);
+
+		String expected1 = prefix + "0".repeat(length - prefix.length() - 1) + "1";
+		assertThat(func.execute(Input.EMPTY)).isEqualTo(expected1);
+	}
+
+	@Test
+	public void testMaxLength() {
+		Function<String> func = new FixedLengthString();
+		func.init(new Input("length", "" + MAX_LENGTH));
+		assertThat(func.execute(Input.EMPTY)).isEqualTo("0".repeat(MAX_LENGTH));
 	}
 
 }
