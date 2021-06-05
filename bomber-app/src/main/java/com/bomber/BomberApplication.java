1c0ffeadb48f1faeaac90005ea5e8817ddd09367
LightMingMing <1849901944@qq.com>
2021-06-06 00:36:01 +0800

    Add app module

---
+++ bomber-app/src/main/java/com/bomber/BomberApplication.java
@@ -0,0 +1,17 @@
+package com.bomber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Bomber 启动入口
+ *
+ * @author MingMing Zhao
+ * @version 2.0.0
+ */
+@SpringBootApplication
+public class BomberApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BomberApplication.class, args);
+	}
+}
