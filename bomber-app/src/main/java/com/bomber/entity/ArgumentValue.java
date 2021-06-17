132c02fbd78bd292ae314ea164c6289ac7750c9f
LightMingMing <1849901944@qq.com>
2021-06-18 00:12:55 +0800

    Add functionConfigure

---
+++ bomber-app/src/main/java/com/bomber/entity/ArgumentValue.java
@@ -0,0 +1,18 @@
+package com.bomber.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 参数值
+ *
+ * @author MingMing Zhao
+ */
+@Getter
+@Setter
+public class ArgumentValue {
+
+	private String name;
+
+	private String value;
+}
