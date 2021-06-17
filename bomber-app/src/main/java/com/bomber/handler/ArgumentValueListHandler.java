776cf1c32f9939d3ee327f79accfbfc0926dcf0c
LightMingMing <1849901944@qq.com>
2021-06-18 00:14:49 +0800

    Crud functionConfigure

---
+++ bomber-app/src/main/java/com/bomber/handler/ArgumentValueListHandler.java
@@ -0,0 +1,13 @@
+package com.bomber.handler;
+
+import java.util.List;
+
+import com.bomber.entity.ArgumentValue;
+
+/**
+ * List&lt;ArgumentValue&gt; 类型处理器
+ *
+ * @author MingMing Zhao
+ */
+public class ArgumentValueListHandler extends JsonTypeHandler<List<ArgumentValue>> {
+}
