e8532588c227ff9732034dc4195ac1cd493e5f35
LightMingMing <1849901944@qq.com>
2021-06-18 19:05:34 +0800

    Crud httpSample

---
+++ bomber-app/src/main/java/com/bomber/handler/AssertionListHandler.java
@@ -0,0 +1,11 @@
+package com.bomber.handler;
+
+import java.util.List;
+
+import com.bomber.entity.Assertion;
+
+/**
+ * @author MingMing Zhao
+ */
+public class AssertionListHandler extends JsonTypeHandler<List<Assertion>> {
+}
---
+++ bomber-app/src/main/java/com/bomber/handler/HttpHeaderListHandler.java
@@ -0,0 +1,11 @@
+package com.bomber.handler;
+
+import java.util.List;
+
+import com.bomber.entity.HttpHeader;
+
+/**
+ * @author MingMing Zhao
+ */
+public class HttpHeaderListHandler extends JsonTypeHandler<List<HttpHeader>> {
+}
