55a90b5314f2e4d99cda8e9d62d836d3875bdc37
LightMingMing <1849901944@qq.com>
2021-06-19 17:07:09 +0800

    Fix missing type

--- bomber-app/src/main/java/com/bomber/handler/ArgumentValueListHandler.java
+++ bomber-app/src/main/java/com/bomber/handler/ArgumentValueListHandler.java
@@ -2,7 +2,10 @@ package com.bomber.handler;
 
 import java.util.List;
 
+import com.bomber.common.util.JsonUtils;
 import com.bomber.entity.ArgumentValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 /**
  * List&lt;ArgumentValue&gt; 类型处理器
@@ -10,4 +13,9 @@ import com.bomber.entity.ArgumentValue;
  * @author MingMing Zhao
  */
 public class ArgumentValueListHandler extends JsonTypeHandler<List<ArgumentValue>> {
+	@Override
+	protected List<ArgumentValue> readValue(String json) throws JsonProcessingException {
+		return JsonUtils.fromJson(json, new TypeReference<>() {
+		});
+	}
 }
