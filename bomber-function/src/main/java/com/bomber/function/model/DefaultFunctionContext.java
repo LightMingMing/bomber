afb36d835fb7ecca60eadbf7c4065afd82b983dd
LightMingMing <1849901944@qq.com>
2021-06-20 21:39:27 +0800

    Fix NPE

--- bomber-function/src/main/java/com/bomber/function/model/DefaultFunctionContext.java
+++ bomber-function/src/main/java/com/bomber/function/model/DefaultFunctionContext.java
@@ -8,11 +8,11 @@ import com.bomber.function.util.FunctionHelper;
 public class DefaultFunctionContext extends AbstractFunctionContext {
 
 	public DefaultFunctionContext(String name, String functionName) {
-		this(name, FunctionHelper.getFunctionMetadata(functionName));
+		this(name, getMetadata(functionName));
 	}
 
 	public DefaultFunctionContext(String name, String functionName, Map<String, String> parameterValues) {
-		this(name, FunctionHelper.getFunctionMetadata(functionName), parameterValues);
+		this(name, getMetadata(functionName), parameterValues);
 	}
 
 	public DefaultFunctionContext(String name, FunctionMetadata metadata) {
@@ -21,5 +21,13 @@ public class DefaultFunctionContext extends AbstractFunctionContext {
 
 	public DefaultFunctionContext(String name, FunctionMetadata metadata, Map<String, String> parameterValues) {
 		super(name, metadata, parameterValues);
+	}
+
+	private static FunctionMetadata getMetadata(String functionName) {
+		FunctionMetadata metadata = FunctionHelper.getFunctionMetadata(functionName);
+		if (metadata == null) {
+			throw new IllegalArgumentException("Not found function '" + functionName + "'");
+		}
+		return metadata;
 	}
 }
