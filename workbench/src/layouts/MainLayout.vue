570689c2f8455f45ea8cc344dcae2834457e3373
LightMingMing <1849901944@qq.com>
2021-07-13 00:05:34 +0800

    Init commit

---
+++ workbench/src/layouts/MainLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header class="bg-grey-1 text-dark">
+      <q-toolbar class="q-px-none">
+        <q-btn
+          flat
+          round
+          icon="menu"
+          aria-label="Menu"
+          color="primary"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Bomber </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="">
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
