43923e88b6de7e7ff209be21e3c653b9121e95be
LightMingMing <1849901944@qq.com>
2021-07-15 23:04:19 +0800

    Allow cross origin

---
+++ bomber-app/src/main/java/com/bomber/controller/ApiConfigurer.java
@@ -0,0 +1,21 @@
+package com.bomber.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Api Configurer
+ *
+ * @author MingMing Zhao
+ */
+@Configuration(proxyBeanMethods = false)
+public class ApiConfigurer implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/*").allowedOriginPatterns("*")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+	}
+
+}
