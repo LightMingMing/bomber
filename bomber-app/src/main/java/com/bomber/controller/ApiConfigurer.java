cd53fd13f953dbfd4b3e8a340d28277eb5aa02ed
LightMingMing <1849901944@qq.com>
2021-07-17 19:33:15 +0800

    Use double asterisk

--- bomber-app/src/main/java/com/bomber/controller/ApiConfigurer.java
+++ bomber-app/src/main/java/com/bomber/controller/ApiConfigurer.java
@@ -14,7 +14,7 @@ public class ApiConfigurer implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/*").allowedOriginPatterns("*")
+		registry.addMapping("/api/**")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
 	}
 
