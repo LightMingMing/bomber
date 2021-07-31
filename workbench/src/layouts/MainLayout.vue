51928b05198de7de173ff2212a5609d19d25ef1d
LightMingMing <1849901944@qq.com>
2021-07-31 11:06:27 +0800

    Add drawer

--- workbench/src/layouts/MainLayout.vue
+++ workbench/src/layouts/MainLayout.vue
@@ -51,6 +51,114 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="">
+      <q-separator />
+      <q-card flat>
+        <q-card-section class="no-padding">
+          <q-item>
+            <q-item-section>My Workspace</q-item-section>
+            <q-item-section side class="q-pl-xs">
+              <q-btn
+                no-caps
+                unelevated
+                color="grey-3"
+                text-color="black"
+                label="New"
+                size="sm"
+              />
+            </q-item-section>
+            <q-item-section side class="q-pl-xs">
+              <q-btn
+                no-caps
+                unelevated
+                color="grey-3"
+                text-color="black"
+                label="Import"
+                size="sm"
+              />
+            </q-item-section>
+          </q-item>
+        </q-card-section>
+      </q-card>
+      <q-separator />
+      <q-list>
+        <q-expansion-item
+          dense
+          default-opened
+          switch-toggle-side
+          label="Restful API"
+          icon="more_vert"
+          header-class="q-py-sm"
+          content-inset-level="0.3"
+        >
+          <q-card>
+            <q-card-section class="no-padding">
+              <q-item clickable dense class="q-py-sm">
+                <q-item-section side style="width: 75px">
+                  <q-badge outline color="blue" label="GET" />
+                </q-item-section>
+                <q-item-section>Query</q-item-section>
+                <q-item-section side>
+                  <q-icon name="more_vert" />
+                </q-item-section>
+              </q-item>
+            </q-card-section>
+          </q-card>
+          <q-card>
+            <q-card-section class="no-padding">
+              <q-item clickable dense class="q-py-sm">
+                <q-item-section side style="width: 75px">
+                  <q-badge outline color="teal" label="POST" />
+                </q-item-section>
+                <q-item-section>Update</q-item-section>
+                <q-item-section side>
+                  <q-icon name="more_vert" />
+                </q-item-section>
+              </q-item>
+            </q-card-section>
+          </q-card>
+
+          <q-card>
+            <q-card-section class="no-padding">
+              <q-item clickable dense class="q-py-sm">
+                <q-item-section side style="width: 75px">
+                  <q-badge outline color="red" label="DELETE" />
+                </q-item-section>
+                <q-item-section>Delete</q-item-section>
+                <q-item-section side>
+                  <q-icon name="more_vert" />
+                </q-item-section>
+              </q-item>
+            </q-card-section>
+          </q-card>
+          <q-card>
+            <q-card-section class="no-padding">
+              <q-item clickable dense class="q-py-sm">
+                <q-item-section side style="width: 75px">
+                  <q-badge outline color="cyan" label="PUT" />
+                </q-item-section>
+                <q-item-section>Put</q-item-section>
+                <q-item-section side>
+                  <q-icon name="more_vert" />
+                </q-item-section>
+              </q-item>
+            </q-card-section>
+          </q-card>
+          <q-card>
+            <q-card-section class="no-padding">
+              <q-item clickable dense class="q-py-sm">
+                <q-item-section side style="width: 75px">
+                  <q-badge outline color="orange" label="PATCH" />
+                </q-item-section>
+                <q-item-section>Patch</q-item-section>
+                <q-item-section side>
+                  <q-icon name="more_vert" />
+                </q-item-section>
+              </q-item>
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+        <q-separator />
+      </q-list>
     </q-drawer>
 
     <q-page-container>
