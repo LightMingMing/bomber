df8c3420448768f1b3599c2bfb94fb9497d12347
LightMingMing <1849901944@qq.com>
2021-08-07 12:41:14 +0800

    Extract component

--- workbench/src/layouts/MainLayout.vue
+++ workbench/src/layouts/MainLayout.vue
@@ -51,73 +51,7 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="">
-      <q-card flat>
-        <q-card-section class="no-padding">
-          <q-item>
-            <q-item-section>My Workspace</q-item-section>
-            <q-item-section side class="q-pl-xs">
-              <q-btn
-                no-caps
-                unelevated
-                color="grey-3"
-                text-color="black"
-                label="New"
-                size="sm"
-              />
-            </q-item-section>
-            <q-item-section side class="q-pl-xs">
-              <q-btn
-                no-caps
-                unelevated
-                color="grey-3"
-                text-color="black"
-                label="Import"
-                size="sm"
-              />
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-      </q-card>
-      <q-separator />
-      <q-list>
-        <q-expansion-item
-          dense
-          default-opened
-          switch-toggle-side
-          icon="more_vert"
-          header-class="q-py-sm"
-          content-inset-level="0.3"
-          v-for="group in groups"
-          :key="group.name"
-        >
-          <template v-slot:header>
-            <q-item-section>
-              {{ group.name }}
-            </q-item-section>
-            <q-item-section side>
-              <q-btn flat dense round icon="more_vert" @click.stop="" />
-            </q-item-section>
-          </template>
-          <q-card v-for="request in group.requests" :key="request.name">
-            <q-card-section class="no-padding">
-              <q-item clickable dense class="q-py-sm">
-                <q-item-section side style="width: 75px">
-                  <q-badge
-                    outline
-                    :label="request.method"
-                    :color="displayColor(request.method)"
-                  />
-                </q-item-section>
-                <q-item-section>{{ request.name }}</q-item-section>
-                <q-item-section side>
-                  <q-btn flat dense round icon="more_vert" @click.stop="" />
-                </q-item-section>
-              </q-item>
-            </q-card-section>
-          </q-card>
-        </q-expansion-item>
-        <q-separator />
-      </q-list>
+      <workspace :name="name" :groups="groups"></workspace>
     </q-drawer>
 
     <q-page-container>
@@ -227,8 +161,10 @@
 <script>
 import { defineComponent, ref } from "vue";
 import { fabGithub } from "@quasar/extras/fontawesome-v5";
+import Workspace from "src/components/Workspace.vue";
 
 export default defineComponent({
+  components: { Workspace },
   name: "MainLayout",
 
   setup() {
@@ -314,24 +250,9 @@ export default defineComponent({
       url: ref(null),
       headers,
       body: ref(null),
+      name: "MyWorkspace",
     };
   },
-  methods: {
-    displayColor(method) {
-      if (method == "GET") {
-        return "blue";
-      } else if (method == "POST") {
-        return "teal";
-      } else if (method == "DELETE") {
-        return "red";
-      } else if (method == "PUT") {
-        return "cyan";
-      } else if (method == "PATCH") {
-        return "orange";
-      } else {
-        return "primary";
-      }
-    },
-  },
+  methods: {},
 });
 </script>
